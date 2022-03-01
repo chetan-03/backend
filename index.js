35651c82feb54bf4f23ac015f7b83f43fc736957
Chetan <cv02@gmail.com>
2022-03-01 17:47:01 +0530

    deploy to heroku

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`iNotebook app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 2000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
